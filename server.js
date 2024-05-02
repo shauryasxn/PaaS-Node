9a2e43a3198febbad578b98a4621847e94569826
Shaurya Saxena <128383459+shauryasxn@users.noreply.github.com>
2024-05-02 15:17:21 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
